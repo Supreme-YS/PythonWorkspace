e91ac23d4ead455dbeb59480396a7de5d1e58711
Supreme-YS <dudtjr1225@gmail.com>
2020-08-24 22:13:43 +0900

    pythonpractice
    python practice

---
+++ practice5.py
@@ -0,0 +1,19 @@
+number = 2 + 3 * 4 #14
+print(number)
+number = number + 2 #16
+print(number)
+
+number += 2 # 18
+print(number)
+
+number *= 2 # 36
+print(number)
+
+number /= 2 # 18
+print(number)
+
+number -= 2 # 16
+print(number)
+
+number %= 2
+print(number)
