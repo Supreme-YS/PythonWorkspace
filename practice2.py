1357e1d0b48bee715807bc79bda3cfd1abb58ead
Supreme-YS <dudtjr1225@gmail.com>
2020-08-24 21:50:43 +0900

    PythonPractice
    PythonPractice

---
+++ practice2.py
@@ -0,0 +1,14 @@
+#문자열자료형
+print('풍선')
+print('나비')
+print('ㅋㅋㅋㅋㅋㅋㅋ')
+print('ㅋ' * 7)
+
+#boolean #참/거짓
+
+print(5 > 10)
+print(5 < 10)
+print(True + 1)
+print(not True)
+print(not False)
+print(not (5 > 10))
---
+++ practice4.py
@@ -0,0 +1,27 @@
+print(1+1) #2
+print(3-2) #1
+print(5*2) #10
+print(6/3) #2
+
+print(2**3) # 2 ^ 3 = 8
+print(5%3) #나머지 구하기 2
+print(10%3) #나머지 구하기 1
+print(5//3) #몫 1
+print(10//3) #몫 3
+
+print( 10 > 3) #True 출력
+print( 4 >= 7) #False 출력
+print( 5 <= 5) #True 출력
+
+print( 3 == 3 ) #앞의 값과 뒤의 값이 똑같은지 확인하는 연산 True 출력
+print( 4 == 2 ) #False 출력
+
+print( 1 != 3) #True 출력
+print(not(1 != 3)) #False 출력 not은 반대의 개념 opposite
+
+print((3 > 0) and (3 < 5)) #True
+print((3 > 0) & (3 < 5)) #True
+
+print((3 > 0) or (3 > 5)) #True 
+print((3 > 0) | (3 > 5)) #True
+
