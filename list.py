451a16cdcc4e2e5a3c304476bb94681f6260d8fe
Supreme-YS <dudtjr1225@gmail.com>
2020-11-10 22:50:23 +0900

    10/11/20 TIL

---
+++ list.py
@@ -0,0 +1,31 @@
+#list
+
+a = [1, 2, 3, 4, 5]
+print(a)
+
+#list_feature
+
+person = ['Jacob', 29, 173, True]
+print(person)
+
+#empty_list
+
+empty = []
+
+#range
+
+b = list(range(10))
+print(b)
+
+#range_list
+
+c = list(range(1, 12))
+print(c)
+
+#range_list2
+d = list(range(1, 12, 2))
+print(d)
+
+#range_list3
+e = list(range(10, 0, -1))
+print(e)
