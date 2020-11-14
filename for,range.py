9fd037f79f8fc941d0c940d1ab252008774f4c0f
Supreme-YS <dudtjr1225@gmail.com>
2020-11-14 22:00:30 +0900

    14/11/20 TIL
    https://supreme-ys.tistory.com/45

---
+++ for,range.py
@@ -0,0 +1,268 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # print 100번 사용
+>>> print('hello, world!')
+hello, world!
+>>> for i in range(100):
+	print('hello, world!')
+
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+hello, world!
+>>> for i in range(100):
+	print('Hello, world', i)
+
+Hello, world 0
+Hello, world 1
+Hello, world 2
+Hello, world 3
+Hello, world 4
+Hello, world 5
+Hello, world 6
+Hello, world 7
+Hello, world 8
+Hello, world 9
+Hello, world 10
+Hello, world 11
+Hello, world 12
+Hello, world 13
+Hello, world 14
+Hello, world 15
+Hello, world 16
+Hello, world 17
+Hello, world 18
+Hello, world 19
+Hello, world 20
+Hello, world 21
+Hello, world 22
+Hello, world 23
+Hello, world 24
+Hello, world 25
+Hello, world 26
+Hello, world 27
+Hello, world 28
+Hello, world 29
+Hello, world 30
+Hello, world 31
+Hello, world 32
+Hello, world 33
+Hello, world 34
+Hello, world 35
+Hello, world 36
+Hello, world 37
+Hello, world 38
+Hello, world 39
+Hello, world 40
+Hello, world 41
+Hello, world 42
+Hello, world 43
+Hello, world 44
+Hello, world 45
+Hello, world 46
+Hello, world 47
+Hello, world 48
+Hello, world 49
+Hello, world 50
+Hello, world 51
+Hello, world 52
+Hello, world 53
+Hello, world 54
+Hello, world 55
+Hello, world 56
+Hello, world 57
+Hello, world 58
+Hello, world 59
+Hello, world 60
+Hello, world 61
+Hello, world 62
+Hello, world 63
+Hello, world 64
+Hello, world 65
+Hello, world 66
+Hello, world 67
+Hello, world 68
+Hello, world 69
+Hello, world 70
+Hello, world 71
+Hello, world 72
+Hello, world 73
+Hello, world 74
+Hello, world 75
+Hello, world 76
+Hello, world 77
+Hello, world 78
+Hello, world 79
+Hello, world 80
+Hello, world 81
+Hello, world 82
+Hello, world 83
+Hello, world 84
+Hello, world 85
+Hello, world 86
+Hello, world 87
+Hello, world 88
+Hello, world 89
+Hello, world 90
+Hello, world 91
+Hello, world 92
+Hello, world 93
+Hello, world 94
+Hello, world 95
+Hello, world 96
+Hello, world 97
+Hello, world 98
+Hello, world 99
+>>> for i in range(5, 12):
+	print('Hello, World', i)
+
+	
+Hello, World 5
+Hello, World 6
+Hello, World 7
+Hello, World 8
+Hello, World 9
+Hello, World 10
+Hello, World 11
+>>> 
+>>> for i in range(0, 10, 2):
+	print('Hello, Python', i)
+
+	
+Hello, Python 0
+Hello, Python 2
+Hello, Python 4
+Hello, Python 6
+Hello, Python 8
+>>> for i in range(10, 0, -1):
+	print('Hello, Supreme', i)
+
+	
+Hello, Supreme 10
+Hello, Supreme 9
+Hello, Supreme 8
+Hello, Supreme 7
+Hello, Supreme 6
+Hello, Supreme 5
+Hello, Supreme 4
+Hello, Supreme 3
+Hello, Supreme 2
+Hello, Supreme 1
+>>> 
+>>> for i in reversed(range(10)) #range에 reversed를 사용하여 숫자를 반대로 뒤집음
+SyntaxError: invalid syntax
+>>> for i in reversed(range(10)
+		  print('Hello, world', i)
+		  
+SyntaxError: invalid syntax
+>>> for i in reversed(range(10)):
+	print('Hello, world', i)
+
+	
+Hello, world 9
+Hello, world 8
+Hello, world 7
+Hello, world 6
+Hello, world 5
+Hello, world 4
+Hello, world 3
+Hello, world 2
+Hello, world 1
+Hello, world 0
+>>> 
