9bf04c05fd47ff999df3fd0c34e57e103d6d87f3
Supreme-YS <dudtjr1225@gmail.com>
2020-11-17 23:33:53 +0900

    17/11/20 TIL
    quiz

--- turtle_2.py
+++
@@ -1,9 +0,0 @@
-Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import turtle as t
->>> t.shape('turtle')
->>> for i in range(4):
-	t.forward(100)
-	t.right(90)
-
->>> 
