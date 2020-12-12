2d505748ff294a2d4ad7e668a988e319ad32b6e7
Supreme-YS <dudtjr1225@gmail.com>
2020-12-12 21:36:38 +0900

    Update practice_1.py

--- practice_1.py
+++ practice_1.py
@@ -1,1 +1,10 @@
-# practice 1
+price = int(input())
+name = input()
+
+if name == 'Cash3000':
+    price = price - 3000
+    print(price)
+
+if name == 'Cash5000':
+    price = price - 5000
+    print(price)
