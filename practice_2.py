f452ad7167b37d9e380227afc197a0a6788051de
Supreme-YS <dudtjr1225@gmail.com>
2020-12-12 21:37:17 +0900

    Update practice_2.py

--- practice_2.py
+++ practice_2.py
@@ -1,1 +1,13 @@
-# practice 2
+korean, english, math, science = map(int, input().split())
+
+if 0 <= korean <= 100 and 0 <= english <= 100 and 0 <= math <= 100 and 0 <= science <= 100:
+    average = float((korean+english+math+science)/4)
+
+    if average >= 80:
+        print('합격')
+
+    else:
+        print('불합격')
+                
+else:
+    print('잘못된 점수')
