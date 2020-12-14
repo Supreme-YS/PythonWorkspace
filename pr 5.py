c91249c9fc478b656621d131e84569925e280bda
Supreme-YS <dudtjr1225@gmail.com>
2020-12-14 21:33:54 +0900

    Update pr 5.py

--- pr 5.py
+++ pr 5.py
@@ -1,1 +1,22 @@
-# pr 5
+height = int(input())
+
+for i in range(height):
+    
+    for j in reversed(range(height)):
+        
+        if j > i:
+            
+            print(' ', end='')
+            
+        else:
+            
+            print('*', end='')
+            
+    for j in range(height):
+
+        if j < i:
+            
+            print('*', end='')
+            
+    print()
+
