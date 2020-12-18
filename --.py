ffba07d46a05d635dd8d041d57b109f0fe65f2b2
Supreme-YS <dudtjr1225@gmail.com>
2020-12-18 17:13:42 +0900

    Update --.py

--- --.py
+++ --.py
@@ -1,2 +1,24 @@
 # 1 practice
 
+height = int(input())
+
+for i in range(height):
+    
+    for j in reversed(range(height)):
+        
+        if j > i:
+            
+            print(' ', end='')
+            
+        else:
+            
+            print('*', end='')
+            
+    for j in range(height):
+
+        if j < i:
+            
+            print('*', end='')
+            
+    print()
+
