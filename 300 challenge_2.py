660cf32701023522ef6e857aae52332711d7d839
Supreme-YS <dudtjr1225@gmail.com>
2020-12-23 13:43:23 +0900

    Update 300 challenge_2.py

--- 300 challenge_2.py
+++ 300 challenge_2.py
@@ -25,4 +25,18 @@ t = "Python"
 print(s+"!"+t) #이건 정답이 아니다
 print(s+"!",t) #이것이 정답이다. # ,(콤마)를 사용하면 자동으로 1 space 확보
 
+# 14 2 + 2 * 3의 실행결과
 
+print(2 + 2 * 3)
+
+# 15
+
+a = 128
+print(type(a))
+
+# 16 문자열을 정수형으로 변환
+
+num_str = "720" 
+num_int = int(num_str) #int(정수형)으로 형태를 변경하고, num_int라는 새로운 변수에 할당
+print(type(num_str))
+print(num_int+1, type(num_int))
