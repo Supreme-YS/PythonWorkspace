7dfcf1899e744fbccfd3f0f0d58dd5b02d6dd018
Supreme-YS <dudtjr1225@gmail.com>
2020-12-23 13:39:12 +0900

    Create 300 challenge_1.py

---
+++ 300 challenge_1.py
@@ -0,0 +1,40 @@
+# 300 CHALLENGE
+
+#1
+
+print("Hello World")
+
+#2
+print("Mary's cosmetics")
+
+#3
+print('신씨가 소리질렀다. "도둑이야".')
+
+#4
+print('"C:\Windows"')
+
+#5 다음 코드를 실행해보고 \t와 \n의 역할을 설명해보세요.
+#\n == 강제개행
+#\t == 1 tab(4칸) 이후 출력
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+
+#6
+print("오늘은", "일요일")
+
+#7
+print('naver;kakao;sk;samsung')
+print("naver","kakao","sk","samsung", sep=";") #sep은 한 칸의 공백대신 넣는 값
+
+#8
+
+print("naver", "kakao", "samsung", sep="/")
+
+#9
+
+print("first", end=""); print("second")
+
+#10
+
+print(5/3)
+
+
