83a2e2e7f7bb949c28c036c9d0b10fe5458ab2ba
Supreme-YS <dudtjr1225@gmail.com>
2020-12-24 11:16:38 +0900

    24/12/20 TIL
    300 challenge

--- 300 challenge_3.py
+++ 300 challenge_3.py
@@ -4,4 +4,19 @@
 
 letters = 'python'
 print(letters[0], letters[2])
- 
+
+# 22 자동차 번호가 다음과 같을 때 뒤에 4자리만 출력하세요.
+
+license_plate = "24가 2210"
+print(license_plate[-4:])
+
+# 23 아래의 문자열에서 '홀' 만 출력하세요.
+
+string = "홀짝홀짝홀짝"
+print(string[0:6:2])
+print(string[::2])
+
+# 24 문자열을 거꾸로 뒤집어 출력하세요.
+
+string = "PYTHON"
+print(string[::-1])
